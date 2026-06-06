8abb4c2983045fc007e09f5dbb9ce3f02352785e
Ayub Jama <Ayubfarah67@gmail.com>
2026-06-06 14:59:40 +0300

    Inject accessible brand theme into every HTML page

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+  const response = await context.next();
+  const contentType = response.headers.get('content-type') || '';
+
+  if (!contentType.toLowerCase().includes('text/html')) return response;
+
+  return new HTMLRewriter()
+    .on('head', {
+      element(element) {
+        element.append('<link rel="stylesheet" href="/css/brand-theme.css" data-afwm-brand-theme="true">', { html: true });
+      }
+    })
+    .transform(response);
+}
